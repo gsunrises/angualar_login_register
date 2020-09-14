2e03b8997ce733fdc6d852df8c7e1303d013fdec
郭旭旭 <gsunrises@163.com>
2020-09-14 10:13:01 +0800

    提交login代码

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppService } from './app.service';
+
+import { FormsModule} from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class AppService {
+    constructor(public http: HttpClient) {};
+
+    
+
+}
+
